701608a6b115fedd1d1e264172883936ade66516
Mayur Padhiyar <padhiyar2341991@gmail.com>
2020-01-09 17:46:13 +0530

    Add project files.

---
+++ GenericRepository/DAL/AllRepository.cs
@@ -0,0 +1,30 @@
+﻿using GenericRepository.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GenericRepository.DAL
+{
+    public class AllRepository<T> : _IAllRepository<T> where T : class
+    {
+        private TestEntities _context;
+        private IDbSet<T> dbEntity;
+
+        public AllRepository()
+        {
+            _context = new TestEntities();
+            dbEntity = _context.Set<T>();
+        }
+        public IEnumerable<T> GetModelList()
+        {
+            return dbEntity.ToList();
+        }
+
+        public void InsertModel(T model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GenericRepository/DAL/_IAllRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericRepository.DAL
+{
+   public interface _IAllRepository<T> where T :class
+    {
+        IEnumerable<T> GetModelList();
+        void InsertModel(T model);
+    }
+}
