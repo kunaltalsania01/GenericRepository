b08aee2c469fa596f8dde0a2fbf4912894b4af2f
kunaltalsania01 <talsania.kunal@gmail.com>
2020-01-09 19:16:02 +0530

    space

--- GenericRepository/DAL/AllRepository.cs
+++ GenericRepository/DAL/AllRepository.cs
@@ -22,7 +22,7 @@ namespace GenericRepository.DAL
             return dbEntity.ToList();
         }
 
-        public void InsertModel(T model)
+        public void InsertModel(T model)  
         {
             throw new NotImplementedException();
         }
